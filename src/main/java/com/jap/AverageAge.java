48a902c2ed0ab52a9e41b4945a1c16670b9d0d68
abhishek.lashley <abhishek.lashley@gmail.com>
2022-08-24 08:01:26 +0530

    coding done

--- src/main/java/com/jap/AverageAge.java
+++ src/main/java/com/jap/AverageAge.java
@@ -1,6 +1,7 @@
 package com.jap;
 
-public class AverageAge {
+public class AverageAge
+{
 
     public String averageAgeCalculator(String[] ageRawData)
     {
@@ -8,15 +9,27 @@ public class AverageAge {
 
         try{
             //Write the logic
-
+            for (int i = 0; i < ageRawData.length; i++) {
+                avg = avg + Integer.parseInt(ageRawData[i]);
+            }
+            avg = avg / ageRawData.length;
             //Handle specific exception
-        }catch (Exception exception)
+        }
+        catch (Exception exception)
         {
             return exception.toString();
         }
         return String.valueOf(avg);
     }
+    public static void main(String[] args) {
+        AverageAge averageAge = new AverageAge();
 
+        String[] age = {"35","26","32","44"};
+        System.out.println(averageAge.averageAgeCalculator(age));
+        String[] age1 = {"35","26","thirty-two","44"};
+        System.out.println(averageAge.averageAgeCalculator(age1));
+
+    }
 
 
 }
